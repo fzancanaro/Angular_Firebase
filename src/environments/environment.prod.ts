d23f1008831b489fe2e0d87374a86382cdea0a83
MyPark <myparksydney@gmail.com>
2018-04-09 11:08:12 +1000

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCb6ZDmv3wDOFXRaEZAZb7Vv8va3xWM6Pk',
+    authDomain: 'ng5bs4fb-571df.firebaseapp.com',
+    databaseURL: 'https://ng5bs4fb-571df.firebaseio.com',
+    projectId: 'ng5bs4fb-571df',
+    storageBucket: '',
+    messagingSenderId: '499669634072'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCb6ZDmv3wDOFXRaEZAZb7Vv8va3xWM6Pk',
+    authDomain: 'ng5bs4fb-571df.firebaseapp.com',
+    databaseURL: 'https://ng5bs4fb-571df.firebaseio.com',
+    projectId: 'ng5bs4fb-571df',
+    storageBucket: '',
+    messagingSenderId: '499669634072'
+  }
+};
